0466ca57834b02b16fae4dc3e6874465269dc675
JPage4 <jesse.page@me.com>
2021-08-26 19:05:22 -0500

    Add project files.

---
+++ DotNetAPI/Controllers/AuthorsController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using PracticeAPI.Helpers;
+using PracticeAPI.Models;
+using PracticeAPI.ResourceParameters;
+
+namespace PracticeAPI.Controllers
+{
+    [ApiController]
+    [Route("api/authors")]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        private readonly IMapper _mapper;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository, IMapper mapper)
+        {
+            _courseLibraryRepository = courseLibraryRepository ??
+                throw new ArgumentNullException(nameof(courseLibraryRepository));
+            _mapper = mapper ??
+                throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpGet()]
+        [HttpHead] // returns the info on a resource but not the body of the request
+        public ActionResult<IEnumerable<AuthorDto>> GetAuthors([FromQuery] AuthorsResourceParameters authorsResourceParameters)
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors(authorsResourceParameters);
+
+            //AutoMapper function that will return collection of authors from the "repo" aka CourseLibraryRepository.GetAuthors()
+            var authors = _mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo);
+            return Ok(authors);
+        }
+
+        [HttpGet ("{authorId}", Name = "GetAuthor")]
+        public IActionResult GetAuthor(Guid authorId)
+        {
+            var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+
+            if (authorFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<AuthorDto>(authorFromRepo));
+        }
+
+        [HttpPost]
+        public ActionResult<AuthorDto> CreateAuthor(AuthorForCreationDto author)
+        {
+            var authorEntity = _mapper.Map<CourseLibrary.API.Entities.Author>(author);
+            _courseLibraryRepository.AddAuthor(authorEntity);
+            _courseLibraryRepository.Save();
+
+            var authorToReturn = _mapper.Map<AuthorDto>(authorEntity);
+            return CreatedAtRoute("GetAuthor", new { authorId = authorToReturn.Id }, authorToReturn);
+        }
+
+        [HttpOptions]
+        public IActionResult GetAuthorOptions()
+        {
+            Response.Headers.Add("Allow", "GET,OPTIONS,POST");
+            return Ok();
+        }
+    }
+}
---
+++ DotNetAPI/Profiles/AuthorsProfile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using PracticeAPI.Helpers;
+
+namespace PracticeAPI.Profiles
+{
+    public class AuthorsProfile : Profile
+    {
+        //Maps data from the Author Entity class (that we want to control consummer visability to) to the AuthoDto (Data transfer object) that the consummer can access
+        public AuthorsProfile()
+        {
+            CreateMap<CourseLibrary.API.Entities.Author, Models.AuthorDto>()
+                .ForMember(
+                    dest => dest.Name,
+                    opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
+                .ForMember(
+                    dest => dest.Age,
+                    opt => opt.MapFrom(src => src.DateOfBirth.GetCurrentAge()));
+
+            CreateMap<Models.AuthorForCreationDto, CourseLibrary.API.Entities.Author>();
+        }
+    }
+}
